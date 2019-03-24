6ca94fccd4a6357a1028449346842806ea7445df
MatangP <gananimatan24@gmail.com>
2019-03-24 15:39:29 +0200

    Updates

---
+++ precache-manifest.4727bba123c3951495c2b7e8432ce717.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "94f0c3f03e1506a599eb",
+    "url": "/contactmanager/static/js/main.cdb973a9.chunk.js"
+  },
+  {
+    "revision": "5c4438e9067ef97b8208",
+    "url": "/contactmanager/static/js/2.20d82ee2.chunk.js"
+  },
+  {
+    "revision": "94f0c3f03e1506a599eb",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "5c4438e9067ef97b8208",
+    "url": "/contactmanager/static/css/2.1e211c7a.chunk.css"
+  },
+  {
+    "revision": "e53c9c4fa77c44123a22b43b15627f44",
+    "url": "/contactmanager/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/contantmanager/precache-manifest.5015e55be0c90457021e6d56a8545d57.js"
+  "/contactmanager/precache-manifest.4727bba123c3951495c2b7e8432ce717.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/contantmanager/index.html", {
+workbox.routing.registerNavigationRoute("/contactmanager/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
